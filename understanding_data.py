ec7c10361314e5344ccd1b8594e4dd9e277f5c23
Sonia Nigam <sonianigam@sbcglobal.net>
2016-11-29 23:47:46 -0600

    problem 1

---
+++ understanding_data.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from mnist import load_mnist
+
+################# Problem 1A: 50 samples of digit 8 ##################
+
+# only returns images of digit 8
+images, labels = load_mnist(digits=[8], path='.')
+
+for i in xrange(50):
+	plt.imshow(images[i], cmap = 'gray')
+	plt.title('Handwritten image of the digit ' + str(labels[i]))
+	plt.show()
+
+################### Problem 1B #####################
+total = 0
+
+#returns length of image set corresponding to each digit
+for x in xrange(0,10):
+	images, labels = load_mnist(digits=[x], path='.')
+	print "The total number of images for digit " + str(x) + ": "+ str(len(images))
+	total += len(images)
+
+print "The total number of images: " + str(total)
